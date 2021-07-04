88071ae33b3e8f8d49798b406acffd52c9f3334c
wnakamur505 <wnakamur@ncc.go.jp>
2021-07-04 14:51:49 +0000

    minor revision

--- GTEx/GTEx_blood.sh
+++ GTEx/GTEx_blood.sh
@@ -1,11 +1,5 @@
 #! /usr/bin/env bash
 
-#s3のマウントが必要（workのマウントは不要だけどやり方知りたい)
-
-× aws s3 cp --recursive s3://wnakamur-tokyo/GTEx.rna.Blood.Wholo_Blood/ /home/ubuntu/environment/raw_data/GTEx.rna.Blood.Wholo_Blood/ --exclude="*" --include="*SJ.*.tab.gz"
-◯ aws s3 cp --recursive s3://wnakamur-tokyo/GTEx.rna.Blood.Whole_Blood/ /home/ubuntu/environment/raw_data/GTEx.rna.Blood.Whole_Blood/ --exclude="*" --include="*.SJ.out.tab.gz"
-#スペルミス
-
 python3 GTEx_mini_haplotypecaller.py > GTEx_mini_haplotypecaller.txt
 python3 GTEx_counting.py > GTEx_counting.txt
 python3 GTEx_SJ_count_and_genotype.py > GTEx_SJ_count_and_genotype.txt
